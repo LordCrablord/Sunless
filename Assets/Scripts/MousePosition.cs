b44b57f8773e845d1a954845f534295a90b8d756
LordCrablord <ostap.kutsyi@gmail.com>
2022-01-28 11:35:11 +0200

    Mouse click on Map

---
+++ Assets/Scripts/MousePosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosition : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private LayerMask groundLayer;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, groundLayer))
+            {
+                transform.position = raycastHit.point;
+            }
+        }
+    }
+}
