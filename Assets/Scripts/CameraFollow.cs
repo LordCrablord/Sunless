b5910317bbe0a19c6bfea8964655e658c6915056
LordCrablord <ostap.kutsyi@gmail.com>
2022-02-08 18:02:25 +0200

    Made Zoom

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,8 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public Vector3 defaultOffset;
+    public Vector3 minOffset;
+    public Vector3 zoomStep;
     public float smoothTime = 0.3f;
     Vector3 offset;
     Vector3 velocity = Vector3.zero;
@@ -17,6 +19,8 @@ public class CameraFollow : MonoBehaviour
 
 	void Update()
     {
+        if (Input.mouseScrollDelta.y != 0)
+            offset = offset + zoomStep * Input.mouseScrollDelta.y * -1;
         transform.position = Vector3.SmoothDamp(transform.position,target.transform.position + offset, ref velocity, smoothTime);
     }
 }
