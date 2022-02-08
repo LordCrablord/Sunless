cc8de02660c074362547e319fdc4f494dd6d2296
LordCrablord <ostap.kutsyi@gmail.com>
2022-02-08 20:54:27 +0200

    Added zoom

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,22 +13,27 @@ public class CameraFollow : MonoBehaviour
     public float smoothTime = 0.3f;
     Vector3 offset;
     Vector3 velocity = Vector3.zero;
+    Vector3 cameraVelocity = Vector3.zero;
 
-	private void Start()
+    private void Start()
 	{
-        //offset = defaultOffset;
+        offset = defaultOffset;
 	}
 
 	void Update()
     {
-        /*if (Input.mouseScrollDelta.y != 0)
+        if (Input.mouseScrollDelta.y != 0)
+        {
             offset = offset + zoomStep * Input.mouseScrollDelta.y * -1;
-        if (offset.y < minOffset.y || offset.z > minOffset.z)
+            mainCamera.transform.localPosition = offset;
+        }
+        //mainCamera.transform.position = Vector3.SmoothDamp(mainCamera.transform.position, target.transform.position + offset, ref cameraVelocity, smoothTime);
+        /*if (offset.y < minOffset.y || offset.z > minOffset.z)
             offset = minOffset;
         if (offset.y > maxOffset.y || offset.z < maxOffset.z)
             offset = maxOffset;*/
 
-        transform.position = Vector3.SmoothDamp(transform.position,target.transform.position + offset, ref velocity, smoothTime);
+        transform.position = Vector3.SmoothDamp(transform.position,target.transform.position, ref velocity, smoothTime);
 
        /* if (Input.GetKeyDown(KeyCode.L))
             offset = defaultOffset;*/
