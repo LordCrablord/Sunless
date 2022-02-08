1345eecca227cc60d0cb700f1f831eb43e5df564
LordCrablord <ostap.kutsyi@gmail.com>
2022-02-08 19:12:16 +0200

    Revert "Undo this plz"
    This reverts commit 58503bb0f19e273386bebb8d25e0a63424277429.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,21 +4,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform mainCamera;
     public Transform target;
     public Vector3 defaultOffset;
     public Vector3 minOffset;
     public Vector3 maxOffset;
     public Vector3 zoomStep;
     public float smoothTime = 0.3f;
-    float objectOffsetY = 13;
     Vector3 offset;
     Vector3 velocity = Vector3.zero;
 
 	private void Start()
 	{
         offset = defaultOffset;
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y+objectOffsetY, target.transform.position.z);
 	}
 
 	void Update()
@@ -30,7 +27,6 @@ public class CameraFollow : MonoBehaviour
         if (offset.y > maxOffset.y || offset.z < maxOffset.z)
             offset = maxOffset;
 
-        mainCamera.transform.position = Vector3.SmoothDamp(mainCamera.transform.position,target.transform.position + offset, ref velocity, smoothTime);
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + objectOffsetY, target.transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position,target.transform.position + offset, ref velocity, smoothTime);
     }
 }
