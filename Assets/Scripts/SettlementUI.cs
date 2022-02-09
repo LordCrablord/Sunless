954dcd224d440ffd060248dbab1d9abd245bdf13
LordCrablord <ostap.kutsyi@gmail.com>
2022-02-09 18:01:22 +0200

    Small prepares for scriptable objects

---
+++ Assets/Scripts/SettlementUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SettlementUI : MonoBehaviour
+{
+    public GameObject titleTMP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetTitle(string title)
+	{
+
+	}
+}
