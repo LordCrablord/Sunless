415b460242aa21a1386f8296fd1690c0420ee0f9
LordCrablord <ostap.kutsyi@gmail.com>
2022-02-09 13:09:50 +0200

    Made Canvas look at camera

---
+++ Assets/Scripts/LookAtCameraUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCameraUI : MonoBehaviour
+{
+    
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward, Camera.main.transform.up);
+    }
+}
