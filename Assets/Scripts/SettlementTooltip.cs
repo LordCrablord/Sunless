77688a54a660e72ddc3bc8dfc3f7ab9f745d4fda
LordCrablord <ostap.kutsyi@gmail.com>
2022-02-09 12:35:19 +0200

    added mouse enter mouse exit proper work

---
+++ Assets/Scripts/SettlementTooltip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettlementTooltip : MonoBehaviour
+{
+	public float tooltipWait = 0.5f;
+
+	private void OnMouseEnter()
+	{
+		StartCoroutine("ShowTooltip");
+	}
+
+	private void OnMouseExit()
+	{
+		StopCoroutine("ShowTooltip");
+	}
+
+	IEnumerator ShowTooltip()
+	{
+		yield return new WaitForSeconds(tooltipWait);
+		print(gameObject.name);
+	}
+}
