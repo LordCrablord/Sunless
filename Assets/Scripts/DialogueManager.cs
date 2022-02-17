c127d2f65e694048ea0662561c9e03afe5a7d49d
LordCrablord <ostap.kutsyi@gmail.com>
2022-02-17 23:16:14 +0200

    JSON, you've won today

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
@@ -8,15 +9,64 @@ public class DialogueManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DialogueData test = JsonUtility.FromJson<DialogueData>(dialoguesDataJSON.text);
-        Debug.Log(test.id);
-        foreach(string t in test.text)
-            Debug.Log(t);
+        DialogueData test;
+
+        //DialogueData test = JsonUtility.FromJson<DialogueData>(dialoguesDataJSON.text);
+        //foreach (string t in test.text)
+        //    Debug.Log(t);
     }
 
-    private class DialogueData
+    private struct DialogueData
 	{
-        public int id;
         public List<string> text;
 	}
+
+    /*public class Demo
+    {
+        public void ReadJSON()
+        {
+            string path = Application.streamingAssetsPath + "/Player.json";
+            string JSONString = File.ReadAllText(path);
+            Player player = JsonUtility.FromJson<Player>(JSONString);
+            Debug.Log(player.Name);
+        }
+    }
+
+    [System.Serializable]
+    public class Player
+    {
+        public string Name;
+        public int Level;
+    }*/
+
+    //2
+    /*
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
+    }*/
 }
