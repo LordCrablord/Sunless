378b64dda8e73da2688c675cf5873b5167d3e411
LordCrablord <ostap.kutsyi@gmail.com>
2022-02-17 22:11:55 +0200

    Simple DialogueManager Script

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] TextAsset dialoguesDataJSON;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DialogueData test = JsonUtility.FromJson<DialogueData>(dialoguesDataJSON.text);
+        Debug.Log(test.id);
+        foreach(string t in test.text)
+            Debug.Log(t);
+    }
+
+    private class DialogueData
+	{
+        public int id;
+        public List<string> text;
+	}
+}
