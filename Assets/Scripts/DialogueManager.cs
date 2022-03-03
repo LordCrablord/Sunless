e7a6b988a24d38a686a5e11384dd272171cd84bc
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-03 17:54:42 +0200

    Info from JSON goes to text

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -3,19 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
     [SerializeField] TextAsset dialoguesDataJSON;
-    // Start is called before the first frame update
+    [SerializeField] TextMeshProUGUI textTMP;
+
+    DialogueData test;
+    DialogueDataItem currentItem;
+    int currentTextIndex;
     void Start()
     {
-        DialogueData test = JsonUtility.FromJson<DialogueData>(dialoguesDataJSON.text);
-        DialogueDataItem currentItem = test.items.Find(t => t.id == 1);
+        test = JsonUtility.FromJson<DialogueData>(dialoguesDataJSON.text);
+        currentItem = test.items.Find(t => t.id == 1);
+        currentTextIndex = 0;
 
         foreach (string t in currentItem.text)
             Debug.Log(t);
+
+        setTextUI();
     }
+
+    void setTextUI()
+	{
+        textTMP.text = currentItem.text[currentTextIndex];
+	}
 
     [Serializable]
     public class DialogueData
