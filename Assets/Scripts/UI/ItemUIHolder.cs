9dc3182ba6fc55b7d6ab5c7306ec088fd030a2ef
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-07 14:11:17 +0200

    Design of item tooltip

---
+++ Assets/Scripts/UI/ItemUIHolder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemUIHolder : MonoBehaviour
+{
+	float tooltipWait = 0.15f;
+	private void OnMouseEnter()
+	{
+		
+	}
+
+	IEnumerator ShowTooltip()
+	{
+		yield return new WaitForSeconds(tooltipWait);
+
+	}
+}
