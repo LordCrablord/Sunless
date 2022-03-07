e8145a21366b7b1def5c55f34de35b129b8eed70
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-07 12:17:01 +0200

    Proto of leveling System

--- Assets/Scripts/Player/PlayerCharacterStats.cs
+++ Assets/Scripts/Player/PlayerCharacterStats.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerCharacterStats : CharacterStats
 {
-    public int level;
     static int xp;
     public int Xp
 	{
@@ -16,7 +16,27 @@ public class PlayerCharacterStats : CharacterStats
 		{
 			xp = Mathf.RoundToInt(
 				(value + AddAllBonuses(additiveBonuses, Stats.XP)) + (value + AddAllBonuses(additiveBonuses, Stats.XP)) * AddAllBonuses(multiplyingBonuses, Stats.XP)
-				); ;
+				);
+			while (xp > levelXpThreshold[level])
+			{
+				level++;
+				Debug.Log(characterName + " is now level " + level);
+			}
 		}
+	}
+
+	public readonly Dictionary<int, int> levelXpThreshold = new Dictionary<int, int>()
+	{
+		{0, 1},
+		{ 1, 100},
+		{ 2, 200},
+		{ 3, 500},
+		{ 4, 800},
+	};
+
+	int level;
+	public int Level
+	{
+		get { return level; }
 	}
 }
