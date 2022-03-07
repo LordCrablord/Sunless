f34cfa7449d259e06000b13db670e89bfeae50ff
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-07 11:42:01 +0200

    Xp is now common for all characters

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,14 +11,18 @@ public class PlayerController : MonoBehaviour
 
         StatModifier stat1 = new StatModifier { modifierFromID = 1, modifierTo = Stats.XP, value = 5 };
         StatModifier stat2 = new StatModifier { modifierFromID = 1, modifierTo = Stats.HP_MAX, value = 0.2f};
-
         characterStats.AddAdditiveModToList(stat1);
         characterStats.AddMultiplyingModToList(stat2);
 
         characterStats.Xp += 5;
         Debug.Log("Xp: " + characterStats.Xp);
+        PlayerCharacterStats otherCharacter = new PlayerCharacterStats();
+        Debug.Log("Xp of other character: " + otherCharacter.Xp);
+
 
         characterStats.Hp += 400;
+        
+
         GameManager.Instance.SetCharacterDataOnUI(characterStats);
     }
 
