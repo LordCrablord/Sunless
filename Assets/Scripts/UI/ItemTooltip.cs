a007017f22c7c972333e9ddfd4376d34abd4fb7c
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-08 21:49:12 +0200

    UI tooltip preparations for details

--- Assets/Scripts/UI/ItemTooltip.cs
+++ Assets/Scripts/UI/ItemTooltip.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ItemTooltip : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI nameTMP;
     [SerializeField] TextMeshProUGUI descriptionTMP;
+    [SerializeField] Image image;
+    [SerializeField] TextMeshProUGUI goldCost;
 
+    [Header("Weapon")]
+    [SerializeField] GameObject weaponObject;
+    [SerializeField] TextMeshProUGUI damageTMP;
+    [SerializeField] TextMeshProUGUI critChanceTMP;
+    [SerializeField] TextMeshProUGUI critValueTMP;
+
+    [Header("Armor")]
+    [SerializeField] GameObject armorObject;
+    [SerializeField] TextMeshProUGUI armorTMP;
     public void SetTooltip(Item item)
 	{
         nameTMP.text = item.itemName;
         descriptionTMP.text = item.description;
+        image.sprite = item.sprite;
+        goldCost.text = item.goldCost + "g.";
 	}
 }
