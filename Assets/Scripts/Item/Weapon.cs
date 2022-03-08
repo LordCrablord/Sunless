a9cd1341b26c3a714126a39351f5a4223950dcf1
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-08 22:07:51 +0200

    Implemented details for object tooltip

--- Assets/Scripts/Item/Weapon.cs
+++ Assets/Scripts/Item/Weapon.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum WeaponType { SWORD, MACE}
-public enum handUsage { ONE_HANDED, TWO_HANDED}
+public enum HandUsage { ONE_HANDED, TWO_HANDED}
 public enum damageType { SLASHING, BLUDGEONING, FIRE}
 
 [CreateAssetMenu(fileName = "New Weapon", menuName = "Items/Weapon")]
 public class Weapon : Item
 {
     public WeaponType weaponType;
-    public handUsage handUsage;
+    public HandUsage handUsage;
     public int damage;
     public float critChance;
     public float critValue;
--- Assets/Scripts/UI/ItemTooltip.cs
+++ Assets/Scripts/UI/ItemTooltip.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class ItemTooltip : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class ItemTooltip : MonoBehaviour
     [SerializeField] TextMeshProUGUI descriptionTMP;
     [SerializeField] Image image;
     [SerializeField] TextMeshProUGUI goldCost;
+    [SerializeField] TextMeshProUGUI tagsTMP;
 
     [Header("Weapon")]
     [SerializeField] GameObject weaponObject;
@@ -26,5 +28,26 @@ public class ItemTooltip : MonoBehaviour
         descriptionTMP.text = item.description;
         image.sprite = item.sprite;
         goldCost.text = item.goldCost + "g.";
+        tagsTMP.text = Enum.GetName(typeof(ItemType), item.itemType);
+            
+		switch (item)
+		{
+            case Weapon w:
+                weaponObject.SetActive(true);
+                damageTMP.text = w.damage.ToString();
+                critChanceTMP.text = w.critChance + "%";
+                critValueTMP.text = w.critValue * 100 + "%";
+                tagsTMP.text += ", "+Enum.GetName(typeof(WeaponType), w.weaponType)
+                    +", " + Enum.GetName(typeof(HandUsage), w.handUsage);
+                break;
+            case Armor a:
+                armorObject.SetActive(true);
+                armorTMP.text = a.armorValue.ToString();
+                tagsTMP.text += ", " + Enum.GetName(typeof(ArmorWeight), a.armorWeight);
+                break;
+            default:
+                Debug.LogError("Not yet implemented");
+                break;
+		}
 	}
 }
