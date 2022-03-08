6620d07b5fa6f4cc84ded7608cee4ab77905e9b5
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-08 12:29:01 +0200

    Wrote script for Item scriptable object

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName ="Items/Item")]
+public class Item : ScriptableObject
+{
+    public int id;
+    public string itemName;
+    public string description;
+    public Sprite sprite;
+    public int goldCost;
+}
