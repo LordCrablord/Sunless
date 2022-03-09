2d2ee4bed0138c67e8d7512f01f46ddaae941d62
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-09 21:04:55 +0200

    Quest and EventtTrigger Managers created

---
+++ Assets/Scripts/Quests/QuestManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : Singleton<QuestManager>
+{
+    [SerializeField] EventTriggerManager triggerManager;
+    public EventTriggerManager TriggerManager { get { return triggerManager; } }
+}
