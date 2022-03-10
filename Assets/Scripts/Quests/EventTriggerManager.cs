c079db626727692d6f11fb24129f19f3364b9608
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-10 20:05:16 +0200

    New eventTriggersForQuests

--- Assets/Scripts/Quests/EventTriggerManager.cs
+++ Assets/Scripts/Quests/EventTriggerManager.cs
@@ -6,7 +6,8 @@ public delegate void Notify();
 public class EventTriggerManager : MonoBehaviour
 {
     public List<int> settlementConditionForbidID = new List<int>();
-    //public List<int> settlementConditionAllowID = new List<int>();
+    
+    public List<int> questPartAllowID = new List<int>();
 
     public event Notify SettlementTriggerListsModified;
     protected virtual void OnSettlementTriggerListsModified()
@@ -23,5 +24,15 @@ public class EventTriggerManager : MonoBehaviour
     {
         settlementConditionForbidID.Remove(triggerID);
         OnSettlementTriggerListsModified();
+    }
+
+    public void AddToQuestPartAllowList(int triggerID)
+	{
+        questPartAllowID.Add(triggerID);
+	}
+
+    public void RemoveFromQuestPartAllowList(int triggerID)
+    {
+        questPartAllowID.Remove(triggerID);
     }
 }
