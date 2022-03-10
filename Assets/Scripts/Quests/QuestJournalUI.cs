ced1c265f3fc483e98ccfd6918937499da6c4a04
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-10 16:18:44 +0200

    Close Journal implemented

---
+++ Assets/Scripts/Quests/QuestJournalUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestJournalUI : MonoBehaviour
+{
+   public void OnCloseButtonClicked()
+	{
+		gameObject.SetActive(false);
+	}
+}
