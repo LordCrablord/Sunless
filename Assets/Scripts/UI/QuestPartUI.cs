44553abc084d9c4671278e7d1fcaa2ffd4e08c3b
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-10 15:35:23 +0200

    QuestPartUI Implemented

---
+++ Assets/Scripts/UI/QuestPartUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuestPartUI : MonoBehaviour
+{
+    [SerializeField] float defaultHeight = 80;
+    [SerializeField] float toggledHeight = 280;
+
+    bool isToggled = false;
+    [SerializeField] TextMeshProUGUI descriptionTMP;
+
+    public void OnQuestPartClicked()
+	{
+        isToggled = !isToggled;
+        descriptionTMP.gameObject.SetActive(isToggled);
+        float currentHeight = isToggled ? toggledHeight : defaultHeight;
+        GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, currentHeight);
+	}
+}
