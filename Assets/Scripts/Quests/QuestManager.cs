812a6dc21733f80c6a35221dd5a389398e4c961c
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-10 13:37:43 +0200

    Made beginnings for Quest System

--- Assets/Scripts/Quests/QuestManager.cs
+++ Assets/Scripts/Quests/QuestManager.cs
@@ -6,4 +6,9 @@ public class QuestManager : Singleton<QuestManager>
 {
     [SerializeField] EventTriggerManager triggerManager;
     public EventTriggerManager TriggerManager { get { return triggerManager; } }
+
+    [SerializeField] QuestDatabase questDatabase;
+
+    public List<Quest> activeQuests;
+    public List<Quest> completedQuests;
 }
---
+++ Assets/Scripts/Quests/QuestPart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "New QuestPart", menuName = "Quests/QuestPart")]
+public class QuestPart : ScriptableObject
+{
+    public int questPartID;
+    public string partTitle;
+    public string description;
+}
