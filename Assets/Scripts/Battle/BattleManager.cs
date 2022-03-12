6cf137f72f23c905280f3f41c149ea81a2c21756
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-12 18:24:54 +0200

    Token now shows general starting info

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BattleManager : Singleton<BattleManager>
 {
+	[SerializeField] BattleUI battleUI;
     public PlayerCharacterStats[] playerPCs = new PlayerCharacterStats[3];
 
+	private void Start()
+	{
+		battleUI.SetToken(playerPCs[0], 0);
+	}
 }
---
+++ Assets/Scripts/Battle/BattleUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleUI : MonoBehaviour
+{
+    public GameObject[] allyTilesTokenUI = new GameObject[3];
+
+    public void SetToken(CharacterStats stat, int position)
+	{
+		allyTilesTokenUI[position].transform.GetChild(0)
+			.gameObject.GetComponent<Token>().SetToken(stat);
+	}
+}
