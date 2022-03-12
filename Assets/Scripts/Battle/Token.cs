cf629f76e066cfdc77671ca3acff0971a524ba70
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-12 18:07:16 +0200

    Character Token UI design

---
+++ Assets/Scripts/Battle/Token.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Token : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI nameTMP;
+    [SerializeField] Image image;
+    [SerializeField] TextMeshProUGUI hpTMP;
+    [SerializeField] Slider healthSlider;
+
+    CharacterStats stat;
+
+    public void SetToken(CharacterStats newStats)
+	{
+        stat = newStats;
+        nameTMP.text = stat.characterName;
+        image.sprite = stat.sprite;
+        hpTMP.text = stat.Hp.ToString() + "/" + stat.HpMax.ToString();
+        healthSlider.value = stat.Hp;
+        healthSlider.maxValue = stat.HpMax;
+    }
+}
