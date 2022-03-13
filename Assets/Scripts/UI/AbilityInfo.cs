87142c26f98443600cc38866c2b83eb16e975af8
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-13 21:04:27 +0200

    AbilityTolltip almost implemented

---
+++ Assets/Scripts/UI/AbilityInfo.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AbilityInfo : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI abilityName;
+    [SerializeField] TextMeshProUGUI description;
+    [SerializeField] GameObject from0;
+    [SerializeField] GameObject from1;
+    [SerializeField] GameObject from2;
+
+    [SerializeField] GameObject ally0;
+    [SerializeField] GameObject ally1;
+    [SerializeField] GameObject ally2;
+
+    [SerializeField] GameObject enemy0;
+    [SerializeField] GameObject enemy1;
+    [SerializeField] GameObject enemy2;
+
+    public void SetAbilityTooltip(Ability ability)
+	{
+        abilityName.text = ability.abilityName;
+        description.text = ability.description;
+
+		if (!ability.allowedFromPosition.Contains(TargetPosition.NONE)&& 
+            ability.allowedFromPosition.Count>0)
+		{
+            if (ability.allowedFromPosition.Contains(TargetPosition.POS_ZERO))
+                from2.SetActive(true);
+            if (ability.allowedFromPosition.Contains(TargetPosition.POS_ONE))
+                from1.SetActive(true);
+            if (ability.allowedFromPosition.Contains(TargetPosition.POS_TWO))
+                from0.SetActive(true);
+        }
+        if (!ability.targetAlly.Contains(TargetPosition.NONE) &&
+            ability.targetAlly.Count > 0)
+        {
+            if (ability.targetAlly.Contains(TargetPosition.POS_ZERO))
+                ally2.SetActive(true);
+            if (ability.targetAlly.Contains(TargetPosition.POS_ONE))
+                ally1.SetActive(true);
+            if (ability.targetAlly.Contains(TargetPosition.POS_TWO))
+                ally0.SetActive(true);
+        }
+        if (!ability.targetEnemy.Contains(TargetPosition.NONE) &&
+            ability.targetEnemy.Count > 0)
+        {
+            if (ability.targetEnemy.Contains(TargetPosition.POS_ZERO))
+                enemy0.SetActive(true);
+            if (ability.targetEnemy.Contains(TargetPosition.POS_ONE))
+                enemy1.SetActive(true);
+            if (ability.targetEnemy.Contains(TargetPosition.POS_TWO))
+                enemy2.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/UI/WeaponAbilityUIHolder.cs
+++ Assets/Scripts/UI/WeaponAbilityUIHolder.cs
@@ -7,10 +7,36 @@ public class WeaponAbilityUIHolder : MonoBehaviour
 {
     WeaponAttackAbility attackAbility;
     [SerializeField] Image image;
-    
-    public void SetWeaponAbilityUI(WeaponAttackAbility ab)
+    [SerializeField] GameObject AbilityInfoPrefab;
+    GameObject abilityInfo;
+	[SerializeField] protected Vector2 itemTooltipOffset = new Vector2(70, -150);
+	float tooltipWait = 0.25f;
+
+	public void SetWeaponAbilityUI(WeaponAttackAbility ab)
 	{
         attackAbility = ab;
         image.sprite = attackAbility.sprite;
+	}
+
+	public void OnMouseEnter()
+	{
+		if (attackAbility != null)
+			StartCoroutine("ShowAbilityInfo");
+	}
+
+	public void OnMouseExit()
+	{
+		StopCoroutine("ShowAbilityInfo");
+		if (abilityInfo != null)
+			Destroy(abilityInfo);
+	}
+
+	protected virtual IEnumerator ShowAbilityInfo()
+	{
+		yield return new WaitForSeconds(tooltipWait);
+		abilityInfo = Instantiate(AbilityInfoPrefab, transform.position, Quaternion.identity);
+		abilityInfo.transform.SetParent(transform.parent.parent, false);
+		abilityInfo.GetComponent<RectTransform>().anchoredPosition = itemTooltipOffset + gameObject.GetComponent<RectTransform>().anchoredPosition;
+		abilityInfo.GetComponent<AbilityInfo>().SetAbilityTooltip(attackAbility);
 	}
 }
