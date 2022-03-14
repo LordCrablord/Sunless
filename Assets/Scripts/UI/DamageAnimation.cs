8a3f2f579c71f96e90b1e1a0759c217a0c8ab240
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-14 23:24:24 +0200

    Implemented animation on Crit

--- Assets/Scripts/UI/DamageAnimation.cs
+++ Assets/Scripts/UI/DamageAnimation.cs
@@ -7,12 +7,29 @@ public class DamageAnimation : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI damageTMP;
 	[SerializeField] Animator animator;
+	[SerializeField] Color damageColor;
+	[SerializeField] Color healColor;
+	[SerializeField] Color damageCritColor;
+	float textSizeOnCrit = 60;
 
    public void StartAnimation(DamageEventArgs e)
 	{
 		damageTMP.text = e.damage.ToString();
+
+		if (e.damage >= 0)
+			damageTMP.color = damageColor;
+		else
+			damageTMP.color = healColor;
+
 		if (e.isCrit)
-			animator.Play("Damage Animation");
+		{
+			damageTMP.color = damageCritColor;
+			damageTMP.fontSize = textSizeOnCrit;
+		}
+			
+
+		if (e.isCrit)
+			animator.Play("Damage Crit Animation");
 		else
 			animator.Play("Damage Animation");
 	}
