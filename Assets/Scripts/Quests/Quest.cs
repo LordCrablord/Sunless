257dd78b594fc78f5b67b08e6345155e81e0a4e7
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-18 22:43:20 +0200

    Added quest rewards for quest

--- Assets/Scripts/Quests/Quest.cs
+++ Assets/Scripts/Quests/Quest.cs
@@ -10,4 +10,13 @@ public class Quest : ScriptableObject
     public string questTitle;
     public string description;
     public List<QuestPart> questParts;
+    public QuestReward questReward;
+}
+
+[System.Serializable]
+public class QuestReward
+{
+    public int gold;
+    public int xp;
+    public List<Item> items;
 }
