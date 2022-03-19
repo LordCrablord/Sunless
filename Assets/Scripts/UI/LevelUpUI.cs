6f224a7e8991f50ab0d3bc6c614be6ce151ffde2
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-19 19:01:42 +0200

    Points are now being spent on levelupUI

--- Assets/Scripts/UI/LevelUpUI.cs
+++ Assets/Scripts/UI/LevelUpUI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class LevelUpUI : MonoBehaviour
 {
@@ -13,14 +14,33 @@ public class LevelUpUI : MonoBehaviour
 
 
     PlayerCharacterStats characterStats;
+    int levelUpPoints;
 
     public void SetLevelUpScreen(PlayerCharacterStats stats)
 	{
         characterStats = stats;
+        levelUpPoints = characterStats.LevelUpPoints;
         levelUpPointsTMP.text = characterStats.LevelUpPoints.ToString();
         strTMP.text = characterStats.Str.ToString();
         dexTMP.text = characterStats.Dex.ToString();
         conTMP.text = characterStats.Con.ToString();
         intTMP.text = characterStats.Int.ToString();
+    }
+
+    public void IncreaseValue(TextMeshProUGUI statTMP)
+	{
+		if (levelUpPoints > 0)
+		{
+            int stat = Int32.Parse(statTMP.text);
+            stat++;
+            statTMP.text = stat.ToString();
+            levelUpPoints--;
+            levelUpPointsTMP.text = (Int32.Parse(levelUpPointsTMP.text) - 1).ToString();
+        } 
+	}
+
+    public void OnReset()
+	{
+        SetLevelUpScreen(characterStats);
     }
 }
