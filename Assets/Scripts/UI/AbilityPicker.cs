a9cef137dbc8b61a917bc799045b0feb9daa4513
LordCrablord <ostap.kutsyi@gmail.com>
2022-03-20 21:42:50 +0200

    Set not equipped ability in picker

--- Assets/Scripts/UI/AbilityPicker.cs
+++ Assets/Scripts/UI/AbilityPicker.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AbilityPicker : MonoBehaviour
@@ -7,10 +8,19 @@ public class AbilityPicker : MonoBehaviour
 
 	public readonly Vector2 inventoryOffset = new Vector2(-248, 0);
 	string parentUICanvasName = "CharacterCanvasUI";
+	[SerializeField] GameObject content;
+	[SerializeField] GameObject abilityHolderWidePrefab;
 
 	public void SetAbilitiesInPicker(PlayerCharacterStats stats)
 	{
-
+		List<SpellAbility> spellAbilities = stats.KnownAbilities.Where(a=>!stats.ActiveAbilities.Contains(a)).ToList();
+		foreach(SpellAbility ability in spellAbilities)
+		{
+			GameObject UIHolderWide = Instantiate(abilityHolderWidePrefab, transform.position, Quaternion.identity);
+			UIHolderWide.transform.SetParent(content.transform, false);
+			UIHolderWide.GetComponent<AbilityUIHolderCharWide>().SetAbilityHolder(ability);
+		}
+	
 	}
 
 	public void OnCancel()
---
+++ Assets/Scripts/UI/AbilityUIHolderCharWide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AbilityUIHolderCharWide : AbilityUIHolderChar
+{
+	[SerializeField] TextMeshProUGUI nameTMP;
+	public override void SetAbilityHolder(Ability a)
+	{
+		base.SetAbilityHolder(a);
+		nameTMP.text = ability.abilityName;
+	}
+
+	protected override IEnumerator ShowAbilityInfo()
+	{
+		yield return new WaitForSeconds(tooltipWait);
+		abilityInfo = Instantiate(AbilityInfoPrefab, transform.position, Quaternion.identity);
+		abilityInfo.transform.SetParent(transform.parent.parent.parent.parent, false);
+		abilityInfo.GetComponent<RectTransform>().anchoredPosition = itemTooltipOffset + gameObject.GetComponent<RectTransform>().anchoredPosition;
+		abilityInfo.GetComponent<AbilityInfo>().SetAbilityTooltip(ability);
+	}
+}
