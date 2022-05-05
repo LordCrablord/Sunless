a469f72dfbde5e300579b30f0e1591c8c2485adb
LordCrablord <ostap.kutsyi@gmail.com>
2022-05-05 18:27:53 +0300

    Items now appear in inventory

---
+++ Assets/Scripts/UI/TradeUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TradeUI : MonoBehaviour
+{
+    [SerializeField] InventoryUI playerInventoryUI;
+    public List<Item> tempItems;
+
+    public void SetTradeUI()
+	{
+        playerInventoryUI.GetComponent<InventoryUI>().SetInventory(tempItems);
+	}
+	private void Start()
+	{
+		SetTradeUI();
+	}
+}
