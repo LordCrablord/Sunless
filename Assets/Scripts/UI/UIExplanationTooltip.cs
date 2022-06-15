0d95bbb90e66e3913e58dd4b08fd37b0704899ae
LordCrablord <ostap.kutsyi@gmail.com>
2022-06-15 18:38:27 +0300

    Made explanation UI prefab

---
+++ Assets/Scripts/UI/UIExplanationTooltip.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIExplanationTooltip : MonoBehaviour
+{
+	[SerializeField] GameObject tooltipPrefab;
+	[SerializeField] float tooltipTimeWait;
+	[SerializeField] Vector2 tooltipOffset;
+	GameObject tooltip;
+	public void OnMouseEnter()
+	{
+		StartCoroutine("ShowTooltip");
+	}
+
+	public void OnMouseExit()
+	{
+		StopCoroutine("ShowTooltip");
+		if (tooltip != null)
+			Destroy(tooltip);
+	}
+
+	IEnumerator ShowTooltip()
+	{
+		yield return new WaitForSeconds(tooltipTimeWait);
+		tooltip = Instantiate(tooltipPrefab, transform.position, Quaternion.identity);
+		tooltip.transform.SetParent(transform.parent, false);
+		tooltip.GetComponent<RectTransform>().anchoredPosition = tooltipOffset + gameObject.GetComponent<RectTransform>().anchoredPosition;
+		//tooltip.GetComponent<ItemTooltip>().SetTooltip(item);
+	}
+}
---
+++ Assets/Scripts/UI/UIExplanationTooltipPrefab.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIExplanationTooltipPrefab : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI tooltipText;
+    public void SetTooltiptext(string s)
+	{
+		tooltipText.text = s;
+	}
+}
